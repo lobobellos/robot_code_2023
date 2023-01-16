5ba48fd67bee5118b195e57c682fa253cda97bab
= <918780@dpsk12.net>
2023-01-16 10:54:05 -0700

    add new constants

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -12,4 +12,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Const {
+  //drive motors
+  public final int fl=1;
+  public final int fr=2;
+  public final int rl=3;
+  public final int rr=4;
+}
