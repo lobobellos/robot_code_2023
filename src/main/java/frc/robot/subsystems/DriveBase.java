bf5045e616388ae352b5a3e167f62c6467fe4822
bakedPotatoLord <918780@dpsk12.net>
2023-02-02 13:44:00 -0700

    create driveBase class

---
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Const.drive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class DriveBase extends SubsystemBase{
+
+  private CANSparkMax fl = new CANSparkMax(drive.fl,MotorType.kBrushless );
+  private CANSparkMax fr = new CANSparkMax(drive.fr,MotorType.kBrushless );
+  private CANSparkMax rl = new CANSparkMax(drive.rl,MotorType.kBrushless );
+  private CANSparkMax rr = new CANSparkMax(drive.rr,MotorType.kBrushless );
+
+  private MecanumDrive driveBase;
+  
+  public DriveBase(){
+
+    fr.setInverted(true);
+    rr.setInverted(true);
+
+    driveBase = new MecanumDrive(fl,fr,rl,rr);
+
+  }
+
+  void drive(double x,double y, double z){
+    driveBase.driveCartesian(x, y, z);
+  }
+
+  void stop(){
+    driveBase.driveCartesian(0,0,0);
+  }
+
+}
