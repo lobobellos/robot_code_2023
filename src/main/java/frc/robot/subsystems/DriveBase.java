8f1abcd5500c047ae0acef04764f70372ab0b0a4
bakedPotatoLord <918780@dpsk12.net>
2023-02-09 14:14:15 -0700

    fix driving

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -3,17 +3,17 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import frc.robot.Const.drive;
+import frc.robot.Const;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class DriveBase extends SubsystemBase{
 
-  private CANSparkMax fl = new CANSparkMax(drive.fl,MotorType.kBrushless );
-  private CANSparkMax fr = new CANSparkMax(drive.fr,MotorType.kBrushless );
-  private CANSparkMax rl = new CANSparkMax(drive.rl,MotorType.kBrushless );
-  private CANSparkMax rr = new CANSparkMax(drive.rr,MotorType.kBrushless );
+  private CANSparkMax fl = new CANSparkMax(Const.drive.fl,MotorType.kBrushless );
+  private CANSparkMax fr = new CANSparkMax(Const.drive.fr,MotorType.kBrushless );
+  private CANSparkMax rl = new CANSparkMax(Const.drive.rl,MotorType.kBrushless );
+  private CANSparkMax rr = new CANSparkMax(Const.drive.rr,MotorType.kBrushless );
 
   private MecanumDrive driveBase;
   
@@ -28,7 +28,7 @@ public class DriveBase extends SubsystemBase{
   }
 
   public void drive(double x,double y, double z){
-    driveBase.driveCartesian(x, y, z);
+    driveBase.driveCartesian(-x, y, z*Const.drive.rotateSensitivity);
   }
 
   public void stop(){
