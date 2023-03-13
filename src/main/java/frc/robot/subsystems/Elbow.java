90f631845b22e20c9b2fd046eddb073b27218313
bakedPotatoLord <918780@dpsk12.net>
2023-03-13 17:04:48 -0600

    add elbow pseudocode

---
+++ src/main/java/frc/robot/subsystems/Elbow.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elbow extends SubsystemBase{
+  
+
+  //create motor controller here (victor SPX)
+
+  //create pwm servo 
+
+  //create encoder
+
+  public Elbow(){
+
+  }
+
+  //create method to set the motorcontroller
+
+  //create method to actuate the servo
+
+  public void periodic(){
+    //put values on smartDashboard
+
+  }
+
+
+}
