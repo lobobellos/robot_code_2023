f04863c9625f1238b5f818f1bf81d5ae5a069027
bakedPotatoLord <918780@dpsk12.net>
2023-03-14 14:13:55 -0600

    change claw to use victorSPX

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,30 +1,28 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Const;
 
 public class Claw extends SubsystemBase{
-  CANSparkMax clawMotor = new CANSparkMax(Const.claw.ID,MotorType.kBrushed );
+  VictorSPX clawMotor = new VictorSPX(Const.claw.ID);
 
-  RelativeEncoder encoder = clawMotor.getEncoder();
+  Encoder encoder = new Encoder(0, 1);
 
   public Claw(){
-    encoder.setPosition(0);
+    encoder.reset();
 
   }
 
   public void periodic(){
-    SmartDashboard.putNumber("encoder position", encoder.getPosition());
+    SmartDashboard.putNumber("encoder position", encoder.getDistance());
   }
 
   public void set(double speed){
-    clawMotor.set(speed);
+    clawMotor.set(VictorSPXControlMode.PercentOutput,speed);
   }
-
 
 }
