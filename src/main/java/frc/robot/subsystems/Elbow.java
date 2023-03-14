55a6136bc957e5b5af680a2969e03509c4b80277
bakedPotatoLord <918780@dpsk12.net>
2023-03-14 13:02:14 -0600

    fix errors

--- src/main/java/frc/robot/subsystems/Elbow.java
+++ src/main/java/frc/robot/subsystems/Elbow.java
@@ -2,11 +2,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Const;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -34,7 +33,6 @@ public class Elbow extends SubsystemBase{
 
   public void periodic(){
     //put values on smartDashboard
-    SmartDashboard.putNumber("Elbow motor speed ", motor.get());
     SmartDashboard.putNumber("Elbow servo ", servo.getPosition());
     SmartDashboard.putNumber("Elbow encoder ", encoder.get());
   }
