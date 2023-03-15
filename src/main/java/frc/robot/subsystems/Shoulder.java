ea66e46cb1f1ac843e4489c39da5b4c3ed9024fa
bakedPotatoLord <918780@dpsk12.net>
2023-03-15 10:37:22 -0600

    switch from controlgroup to followers

--- src/main/java/frc/robot/subsystems/Shoulder.java
+++ src/main/java/frc/robot/subsystems/Shoulder.java
@@ -3,10 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Const;
@@ -19,24 +17,16 @@ public class Shoulder extends SubsystemBase{
    * 2 - fr
    * 3 - rr
    */
+  CANSparkMax shoulder = new CANSparkMax(Const.shoulder.ids.fl, MotorType.kBrushless);
 
   CANSparkMax[] controllers = {
-    new CANSparkMax(Const.shoulder.ids.fl, MotorType.kBrushless),
     new CANSparkMax(Const.shoulder.ids.rl, MotorType.kBrushless),
     new CANSparkMax(Const.shoulder.ids.fr, MotorType.kBrushless),
     new CANSparkMax(Const.shoulder.ids.rr, MotorType.kBrushless)
   };
 
   //encoders
-  RelativeEncoder[] encoders = {
-    controllers[0].getEncoder(),
-    controllers[1].getEncoder(),
-    controllers[2].getEncoder(),
-    controllers[3].getEncoder()
-  };
-
-  //motor group
-  MotorControllerGroup shoulder = new MotorControllerGroup(controllers);
+  RelativeEncoder encoder = shoulder.getEncoder();
 
   SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(
     Const.shoulder.kG,
@@ -50,8 +40,11 @@ public class Shoulder extends SubsystemBase{
   );
 
   public Shoulder(){
-    for(RelativeEncoder e : encoders){
-      e.setPosition(0);
+
+    encoder.setPosition(0);
+
+    for(CANSparkMax s : controllers){
+      s.follow(shoulder);
     }
 
     shoulder.setInverted(true);
@@ -62,8 +55,6 @@ public class Shoulder extends SubsystemBase{
   }
 
   public void periodic(){
-    for(int i=0;i<4;i++){
-      SmartDashboard.putNumber("encoder "+i, encoders[i].getPosition());
-    }
+    SmartDashboard.putNumber("shoulder encoder ", encoder.getPosition());
   }
 }
