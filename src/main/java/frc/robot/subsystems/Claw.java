4bb23cce14b38f355230fc78287d1222fd743b17
bakedPotatoLord <918780@dpsk12.net>
2023-03-15 21:50:10 -0600

    specify encoding type

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,10 +11,9 @@ import frc.robot.Const;
 public class Claw extends SubsystemBase{
   WPI_VictorSPX clawMotor = new WPI_VictorSPX(Const.claw.ID);
 
-  Encoder encoder = new Encoder(0, 1);
+  Encoder encoder = new Encoder(0, 1,false,EncodingType.k1X);
 
   public Claw(){
-    encoder.reset();
     setDefaultCommand(
       new RunCommand(
         ()->clawMotor.stopMotor(),
