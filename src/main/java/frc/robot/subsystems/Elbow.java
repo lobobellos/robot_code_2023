fcfdc6e5d4bb158e89e5a8f783e5a1a329316672
bakedPotatoLord <918780@dpsk12.net>
2023-03-17 17:03:09 -0600

    add PID stuff

--- src/main/java/frc/robot/subsystems/Elbow.java
+++ src/main/java/frc/robot/subsystems/Elbow.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -10,40 +11,52 @@ import frc.robot.Const;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elbow extends SubsystemBase{
-  
-  CANSparkMax motor = new CANSparkMax(Const.elbow.motorID, MotorType.kBrushless);
-
+  //servo
   Servo servo = new Servo(Const.elbow.servoID);
-
+  //sparkmax stuff
+  CANSparkMax motor = new CANSparkMax(Const.elbow.motorID, MotorType.kBrushless);
   SparkMaxPIDController controller = motor.getPIDController();
-
   RelativeEncoder encoder = motor.getEncoder();
+  //for pid control
+  double setpoint = 0;
   
   public Elbow(){
-    // TODO: initialization?
-
-    
+    //reset
+    encoder.setPosition(0);
+    // set PID coefficients
+    controller.setP(Const.shoulder.pidff.kP);
+    controller.setI(Const.shoulder.pidff.kI);
+    controller.setD(Const.shoulder.pidff.kD);
+    controller.setIZone(Const.shoulder.pidff.kIz);
+    controller.setFF(Const.shoulder.pidff.kFF);
+    controller.setOutputRange(
+      Const.shoulder.pidff.minOutput,
+      Const.shoulder.pidff.maxOutput
+    );
   }
 
-  //create method to set the motorcontroller
-  // TODO: establish desired control mode
-  public void setMotor(double speed) {
-    motor.set(speed);
+  public void setSetPoint(double setpoint){
+    controller.setReference(setpoint, ControlType.kPosition);
+    this.setpoint = setpoint;
   }
 
-  //create method to actuate the servo
+  public double getSetPoint(){
+    return setpoint;
+  }
+
+  //method to actuate the servo
   public void setServo(double position) {
     servo.set(position);
   }
 
+  public RelativeEncoder getEncoder(){
+    return encoder;
+  }
+  
   public void periodic(){
     //put values on smartDashboard
     SmartDashboard.putNumber("Elbow servo ", servo.getPosition());
     SmartDashboard.putNumber("Elbow encoder ", encoder.getPosition());
-  }
-
-  public RelativeEncoder getEncoder(){
-    return encoder;
   }
 
 }
