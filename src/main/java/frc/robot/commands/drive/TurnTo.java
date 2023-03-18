b5020215b3fe787a9ddb46714fb9cfd0617f2c5f
bakedPotatoLord <918780@dpsk12.net>
2023-03-18 14:56:05 -0600

    first pass at turning PID loop

--- src/main/java/frc/robot/commands/drive/TurnTo.java
+++ src/main/java/frc/robot/commands/drive/TurnTo.java
@@ -1,40 +1,38 @@
 package frc.robot.commands.drive;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Const;
 import frc.robot.subsystems.DriveBase;
 
-public class TurnTo extends CommandBase{
+public class TurnTo extends RunCommand{
 
   DriveBase db;
 
-  PIDController controller = new PIDController(
+  static PIDController controller = new PIDController(
     Const.drive.pidff.kP,
     Const.drive.pidff.kI,
     Const.drive.pidff.kD
   );
 
-  double setpoint;
+  Rotation2d setpoint;
 
-  public TurnTo(DriveBase db,double setpoint){
-    this.db = db;
-
-    addRequirements(db);
-    
-
-    controller.setTolerance(1, 3);
-  }
-
-  public void execute(){
-    db.drive(
-      0,
-      0,
-      controller.calculate(
-        db.getAngle(),
-        setpoint
-      )
+  public TurnTo(DriveBase db,Rotation2d setpoint){
+    super(
+      ()->{
+        db.drive(
+          0,
+          0,
+          controller.calculate(
+            db.getAngleZ().getDegrees(),
+            setpoint.getDegrees()
+          )
+        );
+      }, 
+      db
     );
+    controller.setTolerance(1, 3);
   }
 
   public boolean isFinished(){
