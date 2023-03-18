6d5fbfc38f3891841f3d08a8de4b63e30f82fc01
bakedPotatoLord <918780@dpsk12.net>
2023-03-17 21:10:03 -0600

    truth

--- code.ts
+++ code.ts
@@ -1,22 +1,4 @@
 
 
-function recurse(bool:boolean){
-  if(bool){
-    console.log(bool)
-  }else{
-    recurse(bool)
-  }
-}
-
-recurse(true)
-
-class Cake{
-  flavor:string
-  layers:number
-
-  constructor(flavor:string,layers:number){
-    this.flavor = flavor
-    this.layers = layers
-  }
-}
+console.log("java sucks")
 
