361e89b4e6fc9aab65085bec72f91030e49bed65
bakedPotatoLord <918780@dpsk12.net>
2023-03-18 19:36:31 -0600

    edit elbow PID slifghtly

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -79,9 +79,9 @@ public final class Const {
     //pid+ff
     public static final class pidff {
       //pid
-      public static final double kP = 0.005;
+      public static final double kP = 0.02;
       public static final double kI = 0;
-      public static final double kD = 0.0005;
+      public static final double kD = 0.005;
       // I-zone
       public static final double kIz = 1;
       //feed forward control
