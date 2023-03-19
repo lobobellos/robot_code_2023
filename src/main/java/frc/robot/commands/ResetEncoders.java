1912b4e1726ddff5946d12490aaaffc27cc6f216
bakedPotatoLord <918780@dpsk12.net>
2023-03-18 22:48:23 -0600

    also reset setpoints

--- src/main/java/frc/robot/commands/ResetEncoders.java
+++ src/main/java/frc/robot/commands/ResetEncoders.java
@@ -8,8 +8,12 @@ public class ResetEncoders extends InstantCommand{
   public ResetEncoders(Shoulder shoulder,Elbow elbow){
     super(
       ()->{
+        //reset encoders
         shoulder.getEncoder().setPosition(0);
         elbow.getEncoder().setPosition(0);
+        //reset setpoints
+        shoulder.setSetPoint(0);
+        elbow.setSetPoint(0);
       }
     );
   }
