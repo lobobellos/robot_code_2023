3988ac68b4c51e2c50fe5c52ebf2850baea350fb
bakedPotatoLord <918780@dpsk12.net>
2023-03-21 16:28:40 -0600

    halfway done with unloadArm command

--- src/main/java/frc/robot/commands/auto/UnloadArm.java
+++ src/main/java/frc/robot/commands/auto/UnloadArm.java
@@ -1,26 +1,42 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.ReleaseAndZero;
+import frc.robot.commands.ResetEncoders;
 import frc.robot.subsystems.Elbow;
 import frc.robot.subsystems.Shoulder;
 
-public class UnloadArm extends SequentialCommandGroup {
+public class UnloadArm extends ParallelCommandGroup {
   public UnloadArm(Shoulder shoulder,Elbow elbow){
     super(
-      new InstantCommand(
-        // TODO: set the values
-        ()->shoulder.setSetPoint(0)
+      new RunCommand(
+        ()->{
+          shoulder.runPID.run();
+          elbow.runPID.run();
+        },
+        shoulder,elbow
       ),
-      new WaitCommand(2),
-      new InstantCommand(
-        // TODO: set the values
-        ()->elbow.setSetPoint(0)
-      ),
-      new WaitCommand(2),
-      new ReleaseAndZero(shoulder, elbow)
+      new SequentialCommandGroup(
+        new ResetEncoders(shoulder, elbow),
+        shoulder.moveTo(2.0),
+        new WaitCommand(2),
+        shoulder.moveTo(3),
+        new WaitCommand(0.75),
+        shoulder.moveTo(4),
+        new WaitCommand(0.75),
+        shoulder.moveTo(5.5),
+        new WaitCommand(0.75),
+        shoulder.moveTo(6.5),
+        new WaitCommand(0.75),
+        //shoulder is horisontal now
+        new ResetEncoders(shoulder, elbow),
+        elbow.moveTo(-2),
+        
+        new WaitCommand(3),
+        shoulder.moveTo(2)
+        )
     );
   }
 }
