50ffe5861c6d5b53f51db82bfef67b1edaf011b0
bakedPotatoLord <918780@dpsk12.net>
2023-03-22 11:15:00 -0600

    add auto selector

--- src/main/java/frc/robot/commands/auto/AutoSelector.java
+++ src/main/java/frc/robot/commands/auto/AutoSelector.java
@@ -1,20 +1,30 @@
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.drive.DriveForwards;
 import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Elbow;
+import frc.robot.subsystems.Shoulder;
 
 public class AutoSelector extends CommandBase {
-  public AutoSelector(DriveBase db){
+  
+  final SendableChooser<Integer> autoChooser= new SendableChooser<Integer>();
+
+  public AutoSelector(DriveBase db,Shoulder shoulder,Elbow elbow){
     CommandScheduler scheduler  =CommandScheduler.getInstance();
 
-    int selected = (int) SmartDashboard.getNumber("auto selector", 0);
+    autoChooser.addOption("right", Integer.valueOf(0));
+    autoChooser.addOption("center", Integer.valueOf(1));
+    autoChooser.addOption("left", Integer.valueOf(2));
+    SmartDashboard.putData("Auto choices", autoChooser);
 
-    switch(selected){
+    switch( (int) autoChooser.getSelected()){
       case 0:
-      scheduler.schedule(new DriveForwards(db, 32));
+      scheduler.schedule(new UnloadArm(shoulder, elbow));
+      //scheduler.schedule(new DriveForwards(db, 32));
       break;
       case 1:
       scheduler.schedule(new DriveForwards(db, 3));
