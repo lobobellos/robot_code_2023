d2821945c7c6dec59296ffad00a02bd494e3b8c3
bakedPotatoLord <918780@dpsk12.net>
2023-03-22 13:04:13 -0600

    second pass at auto selection

--- src/main/java/frc/robot/commands/auto/AutoSelector.java
+++ src/main/java/frc/robot/commands/auto/AutoSelector.java
@@ -11,28 +11,47 @@ import frc.robot.subsystems.Shoulder;
 
 public class AutoSelector extends CommandBase {
   
-  final SendableChooser<Integer> autoChooser= new SendableChooser<Integer>();
+  public static enum AutoType{
+    left,
+    right,
+    center
+  }
+
+  final SendableChooser<AutoType> autoChooser= new SendableChooser<AutoType>();
+
+  final CommandScheduler scheduler=CommandScheduler.getInstance();
+
+  final DriveBase db;
+  final Shoulder shoulder;
+  final Elbow elbow;
 
   public AutoSelector(DriveBase db,Shoulder shoulder,Elbow elbow){
-    CommandScheduler scheduler  =CommandScheduler.getInstance();
 
-    autoChooser.addOption("right", Integer.valueOf(0));
-    autoChooser.addOption("center", Integer.valueOf(1));
-    autoChooser.addOption("left", Integer.valueOf(2));
+    this.db = db;
+    this.shoulder = shoulder;
+    this.elbow = elbow;
+
+    autoChooser.addOption("right", AutoType.right);
+    autoChooser.addOption("center", AutoType.center);
+    autoChooser.addOption("left", AutoType.left);
     SmartDashboard.putData("Auto choices", autoChooser);
 
-    switch( (int) autoChooser.getSelected()){
-      case 0:
+  }
+  
+  @Override
+  public void initialize() {
+    switch(  autoChooser.getSelected().name()){
+      case "left":
       scheduler.schedule(new UnloadArm(shoulder, elbow));
       //scheduler.schedule(new DriveForwards(db, 32));
       break;
-      case 1:
+      case "center":
       scheduler.schedule(new DriveForwards(db, 3));
       break;
-      case 2:
+      case "right":
       scheduler.schedule(); //TODO: make third auto
       break;
-
     } 
+    
   }
 }
